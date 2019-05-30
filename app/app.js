ec9f6f54428a5ec5b55bb1d97ccc70e8be2f07be
astro-nat <nano724@tamu.edu>
2019-05-30 14:20:21 +0000

    Added three.js file and fs library variable in app.js

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var app = express();
+var http = require('http'),
+    fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
